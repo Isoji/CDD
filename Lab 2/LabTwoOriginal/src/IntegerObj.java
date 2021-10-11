3c8c7353d7e884c9c8143fc21a300dabe9331859
Benjamin Tremblay <tremblayben@hotmail.ca>
2021-10-11 20:55:40 +0100

    Added project containing original code from labTwo

---
+++ Lab 2/LabTwoOriginal/src/IntegerObj.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author joe
+ */
+class IntegerObj {
+    int value;
+    IntegerObj(int val) {
+        this.value = val;
+    }
+    int inc(){
+        this.value++;
+        return this.value;
+    }
+}
